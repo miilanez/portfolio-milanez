203cce3952190d37ecebce8fad77942a3f4becf3
Gabriel Milanez <gabrielmilanez99@hotmail.com>
2022-07-20 14:53:06 -0300

    commit portfolio in react

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Routing from './Routing'
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+      <body>
+        <Routing />
+      </body>
+    </div>
+  )
+}
+
+export default App
